77e430ea680443e1bc67159892a8a7f3b3db127b
Aaron Wood <aaronjwood@gmail.com>
2011-08-31 10:32:11 -0400

    Updated tester to reflect new method signatures. Fixed the require() due to the class file name changing

--- tester.php
+++ tester.php
@@ -5,16 +5,16 @@
 </head>
 <body>
 <?php
-require("Form.php");
+require("QForm.php");
 $form = new Form("testform", "GET", "tester.php", true);
-$form->addTextField("Some content...");
+$form->addTextField("textfieldName", "textfieldID", "textfieldValue");
 $form->addTextField();
-$form->addTextArea("TEST");
+$form->addTextArea("textareaName", "textareaID", "textareaValue");
 $form->addTextArea();
-$form->addCheckbox("testcheck", "checkname", "checkid");
-$form->addRadioButton("test1", "I", "checkname");
-$form->addRadioButton("test2", "B", "checkname");
-$form->addPassword("NAME", "ID test", "VALUE");
+$form->addCheckbox("checkboxName", "checkboxID", "checkboxValue");
+$form->addRadioButton("radioOneName", "radioOneID", "radioOneValue");
+$form->addRadioButton("radioTwoName", "radioTwoID", "radioTwoValue");
+$form->addPassword("passwordName", "passwordID", "passwordValue");
 $form->addPassword();
 $form->output();
 ?>
