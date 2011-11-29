cb065c52e1e3cd7f028fe60ebc2cbe31aed211ee
Aaron Wood <aaronjwood@gmail.com>
2011-11-29 15:55:24 -0500

    Added a class for a password element

---
+++ classes/Password.php
@@ -0,0 +1,26 @@
+<?php
+class Password extends Element {
+	
+	private $label;
+	private $name;
+	private $id;
+	private $value;
+
+	public $html;
+	
+	//Set the appropriate attributes based on the parameters
+	function __construct($label, $name, $id, $value) {
+		$this->label = $label;
+		$this->name = $name;
+		$this->id = $id;
+		$this->value = $value;
+		$this->constructElement();
+	}
+	
+	//Checks the attributes, generates a label, and creates the element
+	protected function constructElement() {
+		Util::checkAttributes($this->value,$this->name, $this->id, "password");
+		$this->html .= Util::checkLabel($this->label, "<input type='password'$this->name$this->id$this->value />");
+	}
+}
+?>
